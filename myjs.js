a01e186b807e2c5fa77d7b53116ea80545606fff
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-10-08 10:33:05 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,11 @@
 alert('Hello,fude!');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(Math.sqrt(2)+Math.sqrt(3));
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
