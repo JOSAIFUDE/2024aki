5c344749989e02a31a619cc4237f429e0a04a443
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-10-29 14:37:40 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,4 @@
+function sayhello70(){alert('Hello,'+document.getElementbyld("who70").innerHTML'!');}
+function taro70(){document.getElementbyld('who70').innerHTML='太郎'}
+function hanako68(){document.getElementbyld('who70').innerHTML='花子'}
+                      
