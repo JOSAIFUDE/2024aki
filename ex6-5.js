d8970985955da5fba6d2d82ebf1bc5e2921302a3
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-10-29 12:14:35 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -1,6 +1,6 @@
-var who65 ＝ 'world';
-function sayhello65(){alert('Hello,'+who65+'!');}
-function taro65(){who65 = '太郎';}
-function hanako65(){who65 = '花子';}
+var who65 = 'world';
+function sayhello65() {alert('Hello,'+who65+'!');}
+function taro65(){who65='太郎';}
+function hanako65(){who65='花子';} 
 function taro66(){alert('太郎が来た');}
-function hanako66(){alert('花子が来た');}
+function hanako66(){alert('花子が来た);}
