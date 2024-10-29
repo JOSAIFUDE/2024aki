a7f2bedb25650235437796dedd35841b414bdfd5
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-10-29 13:15:42 +0900

    Update and rename ex6-7.js to ex6-6.js

---
+++ ex6-6.js
@@ -0,0 +1,8 @@
+var who65 = 'world';
+function sayhello65() {alert('Hello,'+who65+'!');}
+function taro65(){who65='太郎';}
+function hanako65(){who65='花子';} 
+function sayhello65() {alert('+who65+が来た'!');}
+function taro66(){who65='太郎';} 
+function hanako66(){who65='花子';} 
+
