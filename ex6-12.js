a552f812dc4eb886942bf969be07bf1b2dd6a591
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-19 10:46:31 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,3 +1,4 @@
 function hyoji(){//表示する文字 var str="一文字ずつ表示します。";
 if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
 else{document.timer.moji.value="";}}
+function startnc(){setlnterval("hoyji()",1000;}
