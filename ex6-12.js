b933e6c217a4bf105cb404ff8bdda923d139cf8e
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-21 13:42:21 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,4 +1,7 @@
-function hyoji(){//表示する文字 var str="一文字ずつ表示します。";
+function hyoji(){var str="一文字ずつ表示します。";var cnt=document.timer.moji.value.length;}
 if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
 else{document.timer.moji.value="";}}
 function startnc(){setlnterval("hoyji()",1000;}
+const str='Mozilla';
+console.log(str.substr(2));//expected output:"oz"
+console.log(str.substr(1,2));//expected output:"zilla"
