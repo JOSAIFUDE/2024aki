a9a19b5feff89618c05bcd9a53d8d7d3b6c8c3e8
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-22 14:32:11 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo').src='./image/bulbon.gif';}
-function imgchange2(){document.getElementById('logo1').src='./image/josai.png';}
+function imgchange2(){document.getElementById('logo').src='./image/josai.png';}
