eaa67310d61fcbd3aef09070c01207f27e050cc5
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-22 14:41:57 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,4 @@
 function imgchange1(){document.getElementById('logo').src='./image/bulbon.gif';}
 function imgchange2(){document.getElementById('logo').src='./image/josai.png';}
+function imgchange3(){document.getElementById('logo').src='./image/bulbon.gif';}
+function imgchange4(){document.getElementById('logo').src='./image/josai.png';}
