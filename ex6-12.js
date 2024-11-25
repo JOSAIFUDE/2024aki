9030f1e3f5d64c1a11326e3cb0d0a2c22d99652e
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-25 22:48:02 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,7 +1,6 @@
-function hyoji(){var str="一文字ずつ表示します。";var cnt=document.timer.moji.value.length;}
-if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
-else{document.timer.moji.value="";}}
-function startnc(){setlnterval("hoyji()",1000;}
-const str='Mozilla';
-console.log(str.substr(2));//expected output:"oz"
-console.log(str.substr(1,2));//expected output:"zilla"
+function hyoji() {//表示する文字var str= "一文字ずつ表示します。"; 
+//テキストボックスの文字数var cnt= document.timer.moji.value.length; 
+//文字が全部表示されているか確認if ( cnt< 11 ) 
+{//現在より1文字多く切り出して表示document.timer.moji.value= str.substr(0,cnt+1); } 
+else {//全て表示されたら、空文字に戻すdocument.timer.moji.value= ""; } } 
+function startfnc() {//関数hyoji()を1000ミリ秒間隔で呼び出すsetInterval("hyoji()",1000); } 
