764d4b6d0937188e7c5c9489ebcfc235c5cfc0f6
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-28 10:01:17 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,19 +1,14 @@
-function hyoji(){
+function hyoji13(){
   //表示する文字
-var str="－文字ずつ表示します。";
+var str13="－文字ずつ表示します。";
   //テキストボックスの文字数
-var cnt=document.timer.moji.value.length;
+var cnt13=document.timer13.moji13.value.length;
 //文字が全部表示されているか確認
-if(cnt<11{
+if(cnt13<11{
   //現在より1文字多く切り出して表示
-  document..timer.moji.value=str.substr(0,cnt+1);}
+  document..timer13.moji13.value=str13.substr(0,cnt13+1);}
   else{
     //全て表示されたら、空文字に戻す
-  document.timer.moji.value="";}}
-function startfnc(){
-  //関数hyoji()を1000ミリ秒間隔で呼び出す
-setlnterval("hyoji()",1000);}
-const strt='Mozilla';
-console.log(str.substr(1,2));
-//expexted output:"oz"
-consolelog(strsubstr(2));
+  document.timer13.moji13.value="";}}
+//setTimeout()を含む関数を呼び出す
+setTimeout("hyoji13()",1000);}
