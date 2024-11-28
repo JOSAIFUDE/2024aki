9ddc04af87b2c525f02c979bb9258a68c31ad8f5
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-11-28 14:24:41 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -6,7 +6,7 @@ var cnt13=document.timer13.moji13.value.length;
 //文字が全部表示されているか確認
 if(cnt13<11){
 //現在より1文字多く切り出して表示
-document..timer13.moji13.value=str13.substr(0,cnt13+1);}
+document.timer13.moji13.value=str13.substr(0,cnt13+1);}
 else{
 //全て表示されたら、空文字に戻す
 document.timer13.moji13.value="";}}
