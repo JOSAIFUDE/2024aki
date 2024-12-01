c8e59472b9ebba1e2194a6e9cdb344a436c76f17
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-12-02 07:04:31 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -1,1 +1,6 @@
+function imgchange3() {document.getElementById('logo3').src = './image/bulboff.gif';
+setTimeout('imgback3()', 3000);}
+
+function imgback3() {document.getElementById('logo3').src = './image/bulbon.gif';
+}
 
