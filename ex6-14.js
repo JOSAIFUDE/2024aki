3f7d279ae2efe8616b281e02f904e32341ceb4f0
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2024-12-02 06:57:23 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,17 +1,18 @@
-function hyoji13(){
-//表示する文字
-var str13="－文字ずつ表示します。";
+function hyoji13() { 
+  //表示する文字
+  var str13 = "一文字ずつ表示します。"; 
   
-//テキストボックスの文字数
-var cnt13=document.timer13.moji13.value.length;
+  //テキストボックスの文字数
+  var cnt13 = document.timer13.moji13.value.length; 
   
-//文字が全部表示されているか確認
-if(cnt13<11){
-//現在より1文字多く切り出して表示
-document.timer13.moji13.value=str13.substr(0,cnt13+1);}
-else{
-//全て表示されたら、空文字に戻す
-document.timer13.moji13.value="";}}
+  //文字が全部表示されているか確認
+  if ( cnt13 < 11 ) { 
+    //現在より1文字多く切り出して表示
+    document.timer13.moji13.value = str13.substr(0,cnt13+1); } 
+  else { 
+    //全て表示されたら、空文字に戻す
+    document.timer13.moji13.value = ""; } 
+  
+  //setTimeout()を含む関数を呼び出す
+  setTimeout("hyoji13()",1000); }
 
-//setTimeout()を含む関数を呼び出す
-setTimeout("hyoji13()",1000);}
