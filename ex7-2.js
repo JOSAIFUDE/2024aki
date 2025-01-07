316aaa67d26ebbb6db16710744d99719c1f8a363
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-07 10:18:11 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,1 +1,3 @@
-function add(){var x=document.getElementById('input1').value;var y=document.getElementById('input2').value;alert(x+y);}
+function add(){var x = Number(document.getElementById('input1').value);
+              var y = Number(document.getElementById('input2').value);
+              alert(x+y);}
