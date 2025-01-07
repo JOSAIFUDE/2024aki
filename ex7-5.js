f925f057f151a656d7938d32bd47f001f8b71d54
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-07 09:24:55 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,1 +1,1 @@
-function nazo1(){if(document.getElementById('answer1').value == '夢'){alert('あたり！')} else{alert('はずれ')}
+function nazo7(){if(document.getElementById('answer7').value == '夢'){alert('あたり！')} else{alert('はずれ')}
