041f78513605ba7e2e2cbd92d46ab8fddbf06f16
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-31 18:11:00 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,1 @@
-unction BMI(){var x = Number(document.getElementById('体重').value);var y = Number(document.getElementById('身長').value);var bmi = x / (y * y);alert(bmi);}
+function BMI(){var x = Number(document.getElementById('体重').value);var y = Number(document.getElementById('身長').value);var bmi = x / (y * y);alert(bmi);}
