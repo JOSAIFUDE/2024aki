59f09f99626e8a989acf0d2846c140440caf091a
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-02-01 17:42:28 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,4 @@
 function bmi(){var x = Number(document.getElementById('体重').value);
               var y = Number(document.getElementById('身長').value);
-               var bmi = x / (y * y);
+               var bmi = (x * 10000) / (y * y);
               alert(bmi);}
